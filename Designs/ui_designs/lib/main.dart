ce64dc3858fd1690a6e28b75e6070b9babb0460c
bilal <abdurrahmankar697@gmail.com>
2022-11-24 05:53:48 +0300

    study alignment

--- Designs/ui_designs/lib/main.dart
+++ Designs/ui_designs/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         //colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
         //.copyWith(secondary: Colors.black45), now that i learned it...
       ),
-      home: EnrtyPage(),
+      home: const EnrtyPage(),
     );
   }
 }
@@ -30,6 +30,37 @@ class EnrtyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(children: <Widget>[
+          SizedBox(
+            height: 40.0,
+          ),
+          FlutterLogo(
+            size: 70.0,
+            textColor: Color.fromARGB(17, 158, 158, 158),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Flutter Team",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.5,
+            height: MediaQuery.of(context).size.height / 3.5,
+            color: Colors.white,
+            alignment: Alignment.topCenter,
+            child: const Text("abc"),
+          )
+        ]),
+      ),
     );
   }
 }
