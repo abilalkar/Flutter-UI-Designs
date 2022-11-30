59ce9fdd206105113d07684da950e7f933227926
bilal <abdurrahmankar697@gmail.com>
2022-12-01 01:30:11 +0300

    create appstyle and add colors

---
+++ Full Apps/notebook_app/lib/style/app_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static Color bgColor = Colors.white;
+  static Color mainColor = Colors.black;
+  static Color accentColor = Colors.blueGrey;
+
+  static List<Color> cardColors = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.red.shade100,
+    Colors.red.shade100,
+    Colors.red.shade100,
+    Colors.red.shade100,
+    Colors.red.shade100,
+    Colors.red.shade100,
+  ];
+}
