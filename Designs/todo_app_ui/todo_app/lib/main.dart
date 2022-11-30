cc9259e173de08217f5ba5f08141bbfad929e1b5
bilal <abdurrahmankar697@gmail.com>
2022-11-30 11:40:33 +0300

    add fonts

--- Designs/todo_app_ui/todo_app/lib/main.dart
+++ Designs/todo_app_ui/todo_app/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
           primarySwatch: Colors.blue,
-          fontFamily: ""),
+          fontFamily: "Poppins"),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
