92486e636672c87da0ae628b0b2a21ef2bfb63e9
bilal <abdurrahmankar697@gmail.com>
2022-12-01 03:11:39 +0300

    add padding

--- Full Apps/notebook_app/lib/screens/home_screen.dart
+++ Full Apps/notebook_app/lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notebook_app/style/app_style.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -15,18 +19,51 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: AppStyle.mainColor,
       appBar: AppBar(
         elevation: 0.0,
-        title: const Text("FireNores"),
+        title: const Text("FireNotes"),
         centerTitle: true,
         backgroundColor: AppStyle.bgColor,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Your Recent Notes",
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your Recent Notes",
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 22.0,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            StreamBuilder<QuerySnapshot>(
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.hasData) {
+                  return GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2));
+                }
+                return Text(
+                  "No notes until now",
+                  style: GoogleFonts.nunito(
+                    color: Colors.white,
+                  ),
+                );
+              },
+              stream:
+                  FirebaseFirestore.instance.collection("Notes").snapshots(),
+            ),
+          ],
+        ),
       ),
     );
   }
