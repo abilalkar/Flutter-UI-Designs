9038c0092d1b7d279196484d04efe1403e265e62
abilalkar <bilal.kar@ug.bilkent.edu.tr>
2022-12-07 01:10:52 +0300

    fixed systematic error, wasting many hours

---
+++ Full_Apps/Yeni klasör/flutter_application_1/lib/style/app_style.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyle {
+  static Color bgColor = Color.fromARGB(255, 63, 113, 189);
+  static Color mainColor = Color.fromARGB(255, 13, 28, 94);
+  static Color accentColor = Colors.blueGrey;
+
+  static List<Color> cardColors = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.blue.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100,
+    Colors.pink.shade100,
+    Colors.blueGrey.shade100,
+  ];
+
+  static TextStyle mainTitle = GoogleFonts.roboto(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle mainContent = GoogleFonts.nunito(
+    fontSize: 16.0,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle dataTitle = GoogleFonts.roboto(
+    fontSize: 13.0,
+    fontWeight: FontWeight.w500,
+  );
+}
