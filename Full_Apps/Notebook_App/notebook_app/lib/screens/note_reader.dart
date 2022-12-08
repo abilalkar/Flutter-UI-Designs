c4248f8c9dacd58f7543bd6ef956c7a9a3a2bf24
abilalkar <bilal.kar@ug.bilkent.edu.tr>
2022-12-08 16:22:40 +0300

    modularize note card into note reader

--- Full_Apps/Notebook_App/notebook_app/lib/screens/note_reader.dart
+++ Full_Apps/Notebook_App/notebook_app/lib/screens/note_reader.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:notebook_app/style/app_style.dart';
 
 class NoteReaderScreen extends StatefulWidget {
   NoteReaderScreen(this.doc, {super.key});
@@ -12,6 +15,37 @@ class NoteReaderScreen extends StatefulWidget {
 class _NoteReaderScreenState extends State<NoteReaderScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int colorId = widget.doc['color_id'];
+    return Scaffold(
+      backgroundColor: AppStyle.cardColors[colorId],
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: AppStyle.cardColors[colorId],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.doc["note_title"],
+            style: AppStyle.mainTitle,
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            widget.doc["creation_date"],
+            style: AppStyle.dataTitle,
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            widget.doc["note_content"],
+            style: AppStyle.mainContent,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+    );
   }
 }
